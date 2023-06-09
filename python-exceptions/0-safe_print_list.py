71ceed5e267bb2a1d72684ea9d70ad00cd7ffb34
root <Nicoou>
2023-06-09 12:24:05 -0700

    task 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(my_list=[], x = 0):
+    i = 0
+    for x in range(0, x):
+        try:
+            print(my_list[x], end="")
+            i += 1
+        except Exception:
+            break
+    print()
+    return i
