f7a1a08cc1858d410bfda2441cb8334c51f33a3f
root <Nicoou>
2023-06-09 07:16:02 -0700

    task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -6,7 +6,3 @@ def no_c(my_string):
             new_string[i] = ''
     new_string = ''.join(new_string)
     return new_string
-
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
