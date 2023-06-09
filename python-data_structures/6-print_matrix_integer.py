ab3d3220a4a16c6e1d0f06a90f81913dfe68d1a3
root <Nicoou>
2023-06-09 08:43:12 -0700

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in i:
-            print(j, end=" ")
+        x = 0
+        for y in i:
+            # this print is for clear the space at the end of the matrix
+            print("{:d}".format(y), end="" if len(i) - 1 == x else " ")
+            x += 1
         print()
