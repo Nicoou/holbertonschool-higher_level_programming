98e43c20a36981313089cbc094f29f591ec3b76d
root <Nicoou>
2023-06-09 11:54:46 -0700

    test push

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = list(my_list)
+    for x in range(len(my_list)):
+        new_list[x] = (False if my_list[x] % 2 != 0 else True)
+    return new_list
+                       
