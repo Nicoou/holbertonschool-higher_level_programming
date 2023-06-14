87e36bde367878379c3dd03741cc177ce299394f
root <Nicoou>
2023-06-14 11:39:40 -0700

    task 2 fixed?

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,9 +7,10 @@ class Square:
     __size = 0
 
     def __init__(self, size=0):
+        """square class"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size >= 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
