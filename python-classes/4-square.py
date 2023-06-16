2aaf396934ec971d042d7da8f79fa6b2e79f3d78
root <Nicoou>
2023-06-16 10:00:16 -0700

    another test

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -4,7 +4,6 @@
 
 class Square:
     """Square"""
-    __size = 0
 
     def __init__(self, size=0):
         """another square"""
@@ -16,14 +15,14 @@ class Square:
         return self._size
 
     @size.setter
-    def __init__(self, size=0):
+    def size(self, value):
         """square class"""
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
 
     def area(self):
         area = self.__size * self.__size
