30f4aeab1f13d1daf05cd0cd269828931998b7ea
root <Nicoou>
2023-06-18 14:27:18 -0700

    task 2 fix

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -35,25 +35,25 @@ class Rectangle:
         else:
             self.__height = value
 
-        def area(self):
-            return self.__height * self.__width
+    def area(self):
+        return self.__height * self.__width
 
-        def perimeter(self):
-            if self.__height != 0 and self.__width != 0:
-                return (self.__width * 2) + (self.__height * 2)
-            else:
-                return 0
+    def perimeter(self):
+        if self.__height != 0 and self.__width != 0:
+            return (self.__width * 2) + (self.__height * 2)
+        else:
+            return 0
 
-        def print(self):
-            if self.__height == 0 or self.__width == 0:
-                return ""
-            print(self)
+    def print(self):
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        print(self)
 
-        def __str__(self):
-            words = ""
-            if self.__heigth == 0 or self.__width == 0:
-                return words
-            for i in range(self.__height):
-                words = words + ('#' * self.__width)
-                words = words + ('\n' if i + 1 < self.__height else '')
+    def __str__(self):
+        words = ""
+        if self.__heigth == 0 or self.__width == 0:
             return words
+        for i in range(self.__height):
+            words = words + ('#' * self.__width)
+            words = words + ('\n' if i + 1 < self.__height else '')
+        return words
