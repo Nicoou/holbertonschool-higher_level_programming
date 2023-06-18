0ba39e969fc1690675415e1277c4d23ceb9f70a7
root <Nicoou>
2023-06-18 16:15:38 -0700

    task 7 fix

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -58,7 +58,7 @@ class Rectangle:
             return ''
 
         for i in range(self.__height - 1):
-                print(str(self.print_symbol) * self.__width)
+            print(str(self.print_symbol) * self.__width)
         print(str(self.print_symbol) * self.__width, end="")
         return ''
 
