29ae524e362d9504e663edb02de16ec3365221d8
root <Nicoou>
2023-06-25 14:49:15 -0700

    task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" task 8 rectangle"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        """method"""
+        self.integer_validator("width", width)
+        __width = width
+        self.integer_validator("height", height)
+        __height = height
+        
