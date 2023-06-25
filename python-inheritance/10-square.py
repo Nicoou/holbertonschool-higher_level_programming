cce47371410f5681013c3e74c23755c97d7e5a59
root <Nicoou>
2023-06-25 15:07:14 -0700

    task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """class of square"""
+    def __init__(self, size):
+        """method"""
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+        def area(self):
+            """method"""
+            return self.__size ** 2
