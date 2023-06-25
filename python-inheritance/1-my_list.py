9339f33dca998bbde435760acebe6200259e4c23
root <Nicoou>
2023-06-25 14:21:57 -0700

    task 1.py and .txt

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,6 +5,6 @@
 class MyList(list):
     """class of an list"""
 
-def print_sorted(self):
-    """print the list"""
-    print(sorted(self))
+    def print_sorted(self):
+        """print the list"""
+        print(sorted(self))
