a89f99ef6c7c2b0d9f12b3e16045c1e859323c11
root <Nicoou>
2023-06-25 14:58:37 -0700

    task 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -9,9 +9,9 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """Method"""
         self.integer_validator("width", width)
-        __width = width
+        self.__width = width
         self.integer_validator("height", height)
-        __height = height
+        self.__height = height
 
         def area(self):
             return self.__width * self.__height
