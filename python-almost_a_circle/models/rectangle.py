15ec3034b37a90668fa3bb94e6c20e53cf4ac475
Nicoou <nf9600362@gmail.com>
2023-06-30 23:38:38 -0700

    task 8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -91,3 +91,18 @@ class Rectangle(Base):
             f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - "
             f"{self.__width}/{self.__height}"
         )
+
+    def update(self, *args):
+        """method used by update date of the class"""
+
+        if args:
+            if len(args) > 4:
+                self.__y = args[4]
+            if len(args) > 3:
+                self.__x = args[3]
+            if len(args) > 2:
+                self.__height = args[2]
+            if len(args) > 1:
+                self.__width = args[1]
+            if len(args) > 0:
+                self.id = args[0]
