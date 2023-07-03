c81bd7aa6926b1399c01d4e63bc1542411747e97
Nicoou <nf9600362@gmail.com>
2023-07-02 18:11:08 -0700

    test base

---
+++ python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" base test """
+import unittest
+from models.base import Base
+
+
+class BaseClass(unittest.Testcase):
+    """ base class """
+    def test_negative_id(self):
+        result = Base(-12)
+        self.assertEqual(result.id, -12)
+
+    def test_auto_assign(self):
+        result = Base()
+        result2 = Base()
+        self.assertEqual(result.id, 2)
+
+    def test_one_to_auto_assign(self):
+        result = Base()
+        result2 = Base()
+        self.assertEqual(result.id, 2)
+
+    def test_id_exist(self):
+        result = Base(2)
+        self.assertEqual(result.id, 2)
+
+    def test_to_json_none(self):
+        result = Base.to_json_string(None)
+        self.assertEqual(result, '[]')
+
+    def test_to_json_brackets(self):
+        result = Base.to_json_string(None)
+        self.assertEqual(result, '[]')
+
+    def test_from_json_none(self):
+        result = Base.from_json_string("[]")
+        self.assertEqual(result, [])
+
+
+if __name__ == '__main__':
+    unittest.main()
